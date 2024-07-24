447f45a57facbb6185ac945f2619816b7b7c6b39
sijiaoh <sijiaoh@outlook.com>
2024-07-24 21:25:58 +0900

    順位が移動速度に影響を与える

--- main.js
+++ main.js
@@ -30,6 +30,10 @@ class Cat {
     return this.getRank() === 1;
   }
 
+  isLast() {
+    return this.getRank() === this.cats.length;
+  }
+
   update() {
     if (this.fallDownCount > 0) {
       this.fallDownCount--;
@@ -41,6 +45,11 @@ class Cat {
     this.fallDownJudgementTimeCount++;
     if (this.fallDownJudgementTimeCount > 100) {
       this.fallDownJudgementTimeCount = 0;
+
+      if (this.speed < 0) {
+        this.randSpeed();
+        return;
+      }
 
       const fallDownProbability = this.isTop() ? 0.6 : 0.3;
       const isFallDown = Math.random() < fallDownProbability;
@@ -69,6 +78,13 @@ class Cat {
 
   randSpeed() {
     this.speed = (1 + Math.random() * 2) * goalPos / 1000;
+
+    if (!!this.cats && this.isTop() && Math.random() < 0.5) {
+      this.speed *= 0.5 * -1;
+    }
+    if (!!this.cats && this.isLast()) {
+      this.speed *= 1.5;
+    }
   }
 
   warpToPos({ x, y }) {
